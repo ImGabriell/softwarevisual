26840125561878e852fa63e8a9f18e369b2b5437
Gabriel <95183526+ImGabriell@users.noreply.github.com>
2022-08-11 21:59:34 -0300

    Update Program.cs

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -9,9 +9,9 @@ namespace Aula02
             Console.WriteLine("Informe uma largura:");
             // double area = CalculaArea(35.0, 3.0);
             // Console.WriteLine(area);
-            double largura = Convert.ToDouble(Console.ReadLine()); // string ou var
+            double largura = Convert.ToDouble(Console.ReadLine()); // double ou var
             Console.WriteLine("Informe uma altura:");
-            double altura = Convert.ToDouble(Console.ReadLine()); // string ou var
+            double altura = Convert.ToDouble(Console.ReadLine()); // double ou var
             double area = CalculaArea(largura, altura);
             Console.WriteLine("A área é: ");
             Console.WriteLine(area);
