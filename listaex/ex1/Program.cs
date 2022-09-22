438b5dab185a6571f12274c4d2ab04cb6ea99caa
Gabriel <95183526+ImGabriell@users.noreply.github.com>
2022-09-22 18:58:18 -0300

    Add files via upload

---
+++ listaex/ex1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+// Escrever um algoritmo que receba a altura e a largura de um retângulo 
+// e calculea sua área.
+
+namespace ex01
+{
+    class Program
+    {
+        
+        static double CalcularArea(double largura, double altura) 
+        {
+            return largura * altura;
+        }
+
+        public static void Main(string[] args)
+        {
+            double largura = 0.0;
+            double altura = 0.0;
+            Console.WriteLine("Bem-vindo á calculadoera de área! XD");
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Insira uma largura");
+                    largura = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (System.Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Agora insira uma altura");
+                    altura = double.Parse(Console.ReadLine()); 
+                    break;
+                }
+                catch (System.Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine("A área é: " + CalcularArea(largura, altura));
+            Console.Write("Pressione 'enter' para sair.");
+            Console.ReadLine();
+        }
+    }
+}
